efcda51659203b6446874ced5499de3375ce6209
Remi-Montagne <remi.montagne91@gmail.com>
2017-10-13 15:00:18 +0200

    latest version of morpion, minimax algorithm v1

---
+++ minimax.py
@@ -0,0 +1,113 @@
+#! /usr/bin/env python3
+# coding : utf-8
+
+"""
+This program code an algorithm for a tic-tac-toe game against the computer
+"""
+########################################
+#             MODULES
+########################################
+
+import random as rd
+import numpy as np
+
+########################################
+#              FUNCTIONS
+########################################
+
+def mini_max(grid, i=0, j=0, active_player=-1):
+    """The minimax recursive algorithm"""
+    moves = []
+    scores = []
+    print(grid)
+    #check the possible next moves for the grid.
+    possible_next_moves = get_possible_next_moves(grid)
+    
+    #if there is game over (i.e. grid full or a player wins), returns the corresponding score
+    if game_over(possible_next_moves, grid):
+        print('end of branch')
+        return (i, j), get_score(grid)
+    #else, a list of posssible next moves and associated scores is recursively generated.
+    #given the active player, the max or min score and associated move will then be returned.
+    else:
+        print('uncomplete grid')
+        next_player = other_player(active_player)
+        for (i, j) in possible_next_moves:
+            moves.append((i, j))
+            possible_grid = get_next_possible_grid(grid, i, j, active_player)
+            move, score = mini_max(possible_grid, i, j, next_player)
+            scores.append(score)
+    print(moves)
+    print(scores)
+    return get_score_to_return(moves, scores, active_player)
+
+def get_possible_next_moves(grid):
+    """Gets the possible next moves given the current grid."""
+    possible_moves = []
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if grid[i, j] == 0:
+                possible_moves.append((i, j))
+    return possible_moves
+
+def game_over(possible_next_moves, grid) :
+    """Returns True if a player wins or if the grid is full."""
+    if not possible_next_moves or winner(grid) != 0:
+        return True
+    else:
+        return False
+
+def other_player(active_player):
+    """Gets the other player."""
+    if active_player == 1:
+        return -1
+    else:
+        return 1
+
+def get_next_possible_grid(grid, i, j, active_player):
+    possible_grid = np.copy(grid)
+    possible_grid[i][j] = active_player
+    return possible_grid
+
+def get_score(grid):
+    """Calculates the score of a state."""
+    if winner(grid) == -1:
+        return 10
+    elif winner(grid) == 1:
+        return -10
+    else:
+        return 0
+
+def winner(grid):
+    """Checks and indicates if the player or the computer made a line.
+    Return 1 if the player did and -1 if the computer did.
+    """
+    trace_inverse = grid[2, 0] + grid[1, 1] + grid[0, 2]
+
+    if 3 in grid.sum(axis=0) or 3 in grid.sum(axis=1) \
+    or grid.trace() == 3 or trace_inverse == 3:
+        return 1
+    elif -3 in grid.sum(axis=0) or -3 in grid.sum(axis=1) \
+    or grid.trace() == -3 or trace_inverse == -3:
+        return -1
+    else:
+        return 0
+
+def get_score_to_return(moves, scores, active_player):
+    if active_player == -1:
+        i = scores.index(max(scores))
+    else:
+        i = scores.index(min(scores))
+    return moves[i], scores[i]
+
+
+########################################
+#                MAIN
+########################################
+
+def main():
+    test_grid = np.array([[1, 0, 0], [-1, 0, 0], [-1, 1, 1]])
+    print('Choisir le coup {}'.format(mini_max(test_grid)))
+
+if __name__ == '__main__':
+    main()
--- morpion.py
+++ morpion.py
@@ -13,6 +13,7 @@ import os
 from sys import exit
 import random as rd
 import numpy as np
+import minimax as mn
 
 ########################################
 #              CLASSES
@@ -26,76 +27,53 @@ class Match:
         """
         self.player1 = self.get_player("X")
         self.player2 = self.get_player("O")
-        self.first = self.get_first_player()
-        self.scores = {
-                        self.player1: 0, 
-                        self.player2: 0
-                        }
+        self.scores = [0, 0]
         self.continue_match = True
     
     def get_player(self, nb):
         """Get the name of the players"""
         return input('Joueur {}: '.format(nb))
     
-    def get_first_player(self):
-        """Get the name of the players who begins"""
-        res = ""
-        while res not in [1, 2, 3]:
-            res = int(input("""Choisissez qui commence:
-1 pour {0}
-2 pour {1}
-3 pour choisir au hasard\n""".format(self.player1, self.player2)))
-            if res not in [1, 2, 3]: 
-                print("Je ne comprends pas cette réponse.")
-            elif res == 2: 
-                self.switch(self.player1, self.player2)
-            elif res == 3: 
-                res = rd.randint(1, 2)
-        return res
-    
-    def switch(self, player1, player2):
-        self.player1 = player2
-        self.player2 = player1
-    
     def play_match(self):
         """The steps of the match"""
         while self.continue_match:
             my_grid = Grid(self.player1, self.player2)
             self.update_score(my_grid.play())
-            self.another_game()
+            self.continue_match = self.another_game()
         self.final_result()
         
     def update_score(self, winner):
         """Once a game has been played, update the final score"""
-        if winner == 1:
-            self.scores[self.player1] += 1
-        elif winner == 2:
-            self.scores[self.player2] += 1
-            return 0
-        
+        self.scores[winner] += 1
+
     def another_game(self):
         """Ask the players if they want to play one more game"""
-        print('{0}: {1} - {2}: {3}'.format(self.player1, self.scores[self.player1], self.player2, self.scores[self.player2]))
-        correct_answer = False
-        while not correct_answer :
+        print('{0}: {1} - {2}: {3}'.format(self.player1, self.scores[0], self.player2, self.scores[1]))
+        while True:
             res = input('Faire une autre partie (Y, N) ? ').upper()
             if res == 'Y' :
-                correct_answer = True
-                self.continue_match = True
+                return True
             elif res == 'N':
-                correct_answer = True
-                self.continue_match = False
+                return False
             else:
                 print("Je n'ai pas compris la réponse.")
 
     def final_result(self):
         """Print the results of the global match"""
-        if self.scores[self.player1] > self.scores[self.player2]:
+        if self.scores[0] > self.scores[1]:
             print('{} est le grand gagnant. Félicitation !'.format(self.player1))
-        elif self.scores[self.player1] < self.scores[self.player2]:
+        elif self.scores[0] < self.scores[1]:
             print('{} est le grand gagnant. Félicitation !'.format(self.player2))
         else:
             print('Match nul. Bravo à tous les 2 !')
+
+
+class Position:
+    
+    def __init__(self, i, j):
+        self.line = i
+        self.col = j
+
 
 class Grid:
     """ The class for the tic-tac-toe grid.
@@ -109,52 +87,71 @@ class Grid:
     def __init__(self, player1, player2):
         """initiation of the grid"""
         self.grid = np.zeros((3,3))
-        self.current_player = 0
-        self.player_names = [player1, player2]
+        self.players = [player1, player2]
+        self.current_player = self.get_first_player()
         self.player_scores = [1, -1]
-        self.winner = 0
+        self.winner = -1
         self.continue_game = True
     
     def play(self):
         """The succession of steps of a game"""
         print(self)
         while self.continue_game:
-            #indicate the player whose turn it is
-            print('{} choisit une case'.format(self.player_names[self.current_player])
-            + '("stop" to quit):')
             #get the case to mark
-            line, col = self.get_case()
-            if line == -1 or col == -1 : break            
+            position = self.get_case()
+            if position.line == -1 or position.col == -1 : break
+                        
             #mark the case
-            self.mark_case(line, col, self.player_scores[self.current_player])
+            self.mark_case(position, self.current_player)
+            
             #process the end of turn
             print(self)
-            self.play_again()
-            if self.play_again : self.change_player()
+            self.check_grid()
+            self.change_player()
             
         print(self.end_of_game_message())
         return self.winner
 
+    def get_first_player(self):
+        """Get the name of the players who begins"""
+        keep_going = True
+        while keep_going:
+            inpt = input('Choisissez qui commence: \
+            \n1 pour {0}\
+            \n2 pour {1}\
+            \n3 pour choisir au hasard\n'.format(self.players[0], self.players[1]))
+            if inpt not in ['1', '2', '3']:
+                print("Je ne comprends pas cette réponse.")
+            elif inpt == '3':
+                res = rd.randint(0, 1)
+                keep_going = False
+            else:
+                res = int(inpt) - 1
+                keep_going = False
+        return res
+
     def get_case(self):
         """Get the case to mark."""
+        #indicate the player whose turn it is
+        print('{} choisit une case'.format(self.players[self.current_player]) \
+        + '("stop" to quit):')
+        
         free_case = False
         while not free_case :
             line = self.player_input('ligne')
-            if line == -1: return (-1, -1)
+            if line == -1: return Position(-1, -1)
 
             col = self.player_input('colonne')
-            if col == -1: return (-1, -1)
+            if col == -1: return Position(-1, -1)
 
             if self.grid[line, col] == 0 :
-                free_case = True
-                return line, col
+                return Position(line, col)
             else:
                 print('Cette case a déjà été marquée. Choisissez-en une autre')
 
     def player_input(self, word) :
         """Get the line or colomn that the player wants to mark."""
-        res = ''
-        while res not in ['1', '2', '3', 'stop'] :
+        while True:
             res = input('  {} (1, 2, ou 3): '.format(word))
             if res == 'stop':
                 return -1
@@ -163,11 +160,11 @@ class Grid:
             else:
                 return int(res)-1
 
-    def mark_case(self, line, col, player):
+    def mark_case(self, position, current_player):
         """Mark a case in the grid."""
-        self.grid[line, col] = player
+        self.grid[position.line, position.col] = self.player_scores[current_player]
 
-    def play_again(self):
+    def check_grid(self):
         """Check if the game must continue or stop and if 
         someone won"""
         #check if their is any case left. If not, stop the game.
@@ -183,21 +180,21 @@ class Grid:
         if 3 in self.grid.sum(axis = 0) or 3 in self.grid.sum(axis = 1) \
         or self.grid.trace() == 3 or trace_inverse == 3 : 
             self.continue_game = False
-            self.winner = 1
+            self.winner = 0
         elif -3 in self.grid.sum(axis = 0) or -3 in self.grid.sum(axis = 1) \
         or self.grid.trace() == -3 or trace_inverse == -3 :
             self.continue_game = False
-            self.winner = 2
+            self.winner = 1
 
     def change_player(self):
         self.current_player = 1 if self.current_player == 0 else 0
 
     def end_of_game_message(self):
         list_messages = ["Match nul.",
-        "{} gagne cette manche.".format(self.player_names[self.winner -1]),
-        "{} gagne cette manche".format(self.player_names[self.winner -1])
+        "{} gagne cette manche.".format(self.players[self.winner]),
+        "{} gagne cette manche".format(self.players[self.winner])
         ]
-        return list_messages[self.winner]
+        return list_messages[self.winner+1]
 
     def __repr__(self):
         res = ""
